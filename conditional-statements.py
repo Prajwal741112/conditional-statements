67abf9d908e1aafdfbefd620d256aa5f53214f81
Prajwal G Hondad <yourgithubemail@gmail.com>
2026-02-26 19:22:59 +0530

    updated conditional statements program

--- conditional-statements.py
+++ conditional-statements.py
@@ -48,4 +48,70 @@
 # else:
 #     print("fail")
 
+# amount=int(input("Enter the amount"))
+# if amount<1000:
+#     total=amount-amount*0.1
+# elif amount>=1000 and amount<5000:
+#     total=amount-amount*0.15
+# elif amount>=5000 and amount<10000:
+#     total=amount-amount*0.2
+# else:
+#     total=amount-amount*0.25
+
+# print("pay",total)
+
+
+# dayno=int(input("Enter the number"))
+# if dayno==0:
+#     print("monday")
+# elif dayno==1:
+#     print("tuesday")
+# elif dayno==2:
+#     print("wednesday")
+# elif dayno==3:
+#     print("thursday")
+# elif dayno==4:
+#     print("friday")
+# elif dayno==5:
+#     print("saturday")
+# else:
+#     print("invalid day number")
+
+
+# monthno=int(input("Enter the number"))
+# if monthno==0:
+#     print("janury")
+# elif monthno==1:
+#     print("febuary")
+# elif monthno==2:
+#     print("march")
+# elif monthno==3:
+#     print("april")
+# elif monthno==4:
+#     print("may")
+# elif monthno==5:
+#     print("june")
+# elif monthno==6:
+#     print("july")
+# elif monthno==7:
+#     print("auguest")
+# elif monthno==8:
+#     print("suptember")
+# elif monthno==9:
+#     print("octumber")
+# elif monthno==10:
+#     print("november")
+# elif monthno==11:
+#     print("december")
+
+# else:
+#     print("invalid day number")
+
+
+# year = int(input("Enter the year: "))
+
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("Leap Year")
+# else:
+#     print("Not a Leap Year")
 
