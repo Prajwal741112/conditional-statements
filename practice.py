bd8760751b03211046125af638aaaf5227f2edb0
Prajwal G Hondad <yourgithubemail@gmail.com>
2026-03-02 23:54:44 +0530

    Added practice.py

---
+++ practice.py
@@ -0,0 +1,56 @@
+#Write a program to check whether a number is even or odd.
+
+# n= int(input("enter the number "))
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+#Find the largest of two numbers.
+# n1= int(input("enter the first number "))
+# n2= int(input("enter the second number "))
+# if n1>n2:
+#     print(n1 is greater)
+# else :
+#     print("n2 is largest")
+
+
+#Check Positive, Negative or Zero
+# n= int(input("enter the number "))
+# if n>0:
+#     print("positive")
+# elif n<0:
+#     print("negative")
+# else:
+#     print("zero")
+
+
+#Print Numbers 1 to 10
+# n=1
+# while n<=10:
+#     print(n)
+#     n+=1
+
+
+#Sum of First N Numbers
+# n= int(input("enter the number "))
+# i=1
+# total=0
+# while i<=n:
+#     total+=i
+#     i+=1
+# print("sum",total)
+
+
+
+# Reverse a Number
+# n = int(input("Enter number: "))
+# rev = 0
+
+# while n > 0:
+#     digit = n % 10
+#     rev = rev * 10 + digit
+#     n = n // 10
+
+# print("Reverse =", rev)
