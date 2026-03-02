692c64dd6dad16be307a266aac9b0772d5b11495
Prajwal G Hondad <yourgithubemail@gmail.com>
2026-03-03 00:51:52 +0530

    Added main1.py

---
+++ main1.py
@@ -0,0 +1,62 @@
+# i=0
+# while True:
+#     i=i+1
+#     if i>10:
+#         break
+#     print(i)
+
+
+# i=0
+# while i<5:
+#     i=i+1
+#     if i==3:
+#         break
+#     print(i)
+# else:
+#     print('end of loop')
+#     print('end of program')
+
+# for loop
+# s1 ='python '
+# for i in s1:
+#     print(i)
+
+
+# n=int(input('enter the number'))
+# sum=0
+# for i in range (1 ,n+1):
+#     sum=sum+i
+#     print(sum)
+
+
+
+
+# n=int(input('enter the number'))
+# factorial = 1
+# for i in range (1 ,n+1):
+#     factorial*=i
+# print(factorial)
+
+
+# n=int(input('enter the number'))
+# a=0
+# b=1
+# for i in range(0,n):
+#     c=a+b
+#     a=b
+#     b=c
+#     print(a)
+    
+
+# n = int(input("Enter number: "))
+
+# count = 0
+
+# for i in range(2, n):
+#     if n % i == 0:
+#         count += 1
+
+# if n > 1 and count == 0:
+#     print("Prime")
+# else:
+#     print("Not Prime")
